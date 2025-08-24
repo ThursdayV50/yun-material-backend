e93b307bc60d38216c7638d177fd25813276566f
ThursdayV50 <1479169422@qq.com>
2025-08-24 21:50:37 +0800

    feat: 添加空间管理和用户管理以及AI扩图功能 - 删除多余的请求类和相应类

---
+++ src/main/java/com/hpu/yunmaterialbackend/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.hpu.yunmaterialbackend.controller;
+
+import com.hpu.yunmaterialbackend.common.BaseResponse;
+import com.hpu.yunmaterialbackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+
+}
---
+++ src/test/java/com/hpu/yunmaterialbackend/YunMaterialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpu.yunmaterialbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunMaterialBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
